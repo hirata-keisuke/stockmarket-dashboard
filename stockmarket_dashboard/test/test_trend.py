c2a1b3c5a84f99c72bae073a5f48454c3c218ec0
hirata-keisuke <plainpeace39th@gmail.com>
2022-12-21 01:03:37 +0900

    calc_LRIのテストを追加

--- stockmarket_dashboard/test/test_trend.py
+++ stockmarket_dashboard/test/test_trend.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from unittest import TestCase
-from app.trend import calc_SMA, calc_EMA
+from app.trend import calc_SMA, calc_EMA, calc_LRI
 
 class TestSMA(TestCase):
     def test_simply_moving_average(self):
@@ -21,4 +21,14 @@ class TestSMA(TestCase):
         self.assertEqual(
             [None,None,1.0,2.0,3.0],
             calc_EMA(df, 3)
+        )
+
+    def test_linear_regression_indicator(self):
+
+        nums = [i for i in range(5)]
+        df = pd.DataFrame(data={"終値":nums})
+
+        self.assertEqual(
+            [None,None,2.0,3.0,4.0],
+            calc_LRI(df, 3)
         )
