f45f0885cceffd5ee415ebbe5a303d833caa130b
hirata-keisuke <plainpeace39th@gmail.com>
2022-12-23 23:23:01 +0900

    trand.pyにdocstringを追加

--- stockmarket_dashboard/app/trend.py
+++ stockmarket_dashboard/app/trend.py
@@ -1,8 +1,19 @@
 from sklearn.linear_model import LinearRegression
 
-trend_calculators = {}
+trend_calculators = {} # トレンド分析を計算する関数を格納する辞書
 
 def trend_calculator(name):
+    """トレンド分析を計算する関数を登録する
+
+    デコレータとして使い、nameがキーで関数がバリューの辞書を作る
+
+    Args:
+        name (str): トレンド分析の名前
+
+    Returns:
+        function: 
+    
+    """
 
     def _trend_calculator(calc_fn):
         trend_calculators[name] = calc_fn
@@ -11,10 +22,29 @@ def trend_calculator(name):
     return _trend_calculator
 
 def select_calculator(name):
+    """トレンド分析の手法を選択する
+
+    Args:
+        name (str): 手法の名前
+
+    Returns:
+        function: トレンド分析を計算する関数
+    
+    """
     return trend_calculators[name]
 
 @trend_calculator("simple")
 def calc_SMA(df, num):
+    """単純移動平均線(Simple Moving Average)を計算する
+
+    Args:
+        df (pandas.DataFrame): 株価情報（日付、始値、終値、高値、安値、出来高）
+        num (int): 移動平均を取る期間
+    
+    Returns:
+        list: dfが持つ日付期間で単純移動平均を計算した値を格納したリスト
+    
+    """
     averages = []
     for i in range(len(df)):
         if i >= num-1:
@@ -25,7 +55,16 @@ def calc_SMA(df, num):
 
 @trend_calculator("exponential")
 def calc_EMA(df, num):
+    """指数平滑移動平均線(Exponential Moving Average)を計算する
 
+    Args:
+        df (pandas.DataFrame): 株価情報（日付、始値、終値、高値、安値、出来高）
+        num (int): 移動平均を取る期間
+    
+    Returns:
+        list: dfが持つ日付期間で指数平滑移動平均を計算した値を格納したリスト
+    
+    """
     averages = []
     for i in range(len(df)):
         if i == num-1:
@@ -40,12 +79,23 @@ def calc_EMA(df, num):
 
 @trend_calculator("regression")
 def calc_LRI(df, num):
+    """線形回帰値線(Linear Regression Indicator)を計算する
 
+    その日からnum-1日前のデータで線形回帰を行い、その日の値を推論した値
+
+    Args:
+        df (pandas.DataFrame): 株価情報（日付、始値、終値、高値、安値、出来高）
+        num (int): 線形回帰のデータを取る期間
+    
+    Returns:
+        list: dfが持つ日付期間で線形回帰値を計算した値を格納したリスト
+    
+    """
     averages = []
     for i in range(len(df)):
         if i >= num-1:
             model = LinearRegression()
-            X = [[i] for i in range(num)]
+            X = [[j] for j in range(num)]
             y = df.loc[i-num+1:i,"終値"].to_list()
             model.fit(X,y)
             averages.append(model.predict([X[-1]])[0])
